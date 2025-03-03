d70fee7e1fb579cdc7222a9d1432b5cc991e10b2
Abbasov Mikael <abb.mika3l@gmail.com>
2025-03-03 21:45:46 +0400

    INPUT PARAMS

---
+++ main.py
@@ -0,0 +1,41 @@
+# INPUT PARAMETRS
+# N3
+# V_kr = 930 H = 11500 I = 5000 n = 200
+# 1)Airbus A320 2)Boeing 757-200 3)Airbus A200B2 4) AB-1
+m_0 = [93500, 115680, 133000]
+m_comm = [22000, 45000, 45000]
+m_k = [28000, 42000, 44000]
+m_cy = [8000, 9000, 14000]
+m_t = [24000, 38000, 40000]
+m_cl = [12000, 22000, 22000]
+m_ob = [6000, 7500, 10000]
+m_kr = [7000, 12000, 13000]
+m_sh = [4000, 5000, 7000]
+m_op = [5000, 6000, 7000]
+m_f = [15000, 18000, 22000]
+m_r_comm = []
+m_r_k = []
+m_r_cy = []
+m_r_t = []
+m_r_ob = []
+m_r_kr = []
+m_r_sh = []
+m_r_op = []
+m_r_f = []
+
+
+def calculations():
+    pass
+
+
+def output():
+    pass
+
+
+def main():
+    calculations()
+    output()
+
+
+if __name__ == "__main__":
+    main()
