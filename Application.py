fd035010dd665c50dd4978c4cdf3c7e1fdcdccef
Abbasov Mikael <abb.mika3l@gmail.com>
2025-03-11 06:46:16 +0400

    Web Application added

---
+++ Application.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import main
+
+
+st.write("Working")
--- tables.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-if __name__ == "__main__":
-    d = {"A": 1,
-         "B": 2,
-         "C": 4}
-    data = pd.DataFrame(d, index=list("123"), columns=["A320", "B757", "A300"])
-    print(data)
